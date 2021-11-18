f10aea6720e55455db63127ee6e0e35706e3d726
MiDo1806 <81757415+MiDo1806@users.noreply.github.com>
2021-11-18 14:50:09 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("Hello Winc Academy");
+let name = ("Michel");
+console.log(name);
+console.log(4 + 4);
+console.log(8 * 8);
+console.log(25 - 14);
+console.log(25 / 6)
+console.log(25 % 50);
+let age = (52);
+console.log(age);
+typeof age
+age = "tweeënvijftig"
+console.log(age)
